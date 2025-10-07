70b6cfad54d26895524d261390eed804aecf0f15
沈艺 <shenyi.box1@163.com>
2025-10-07 15:41:54 +0800

    移除了app校验，添加了Celery和Redis校验

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -13,7 +13,10 @@ def name_check(name:str,content:str):
         sys.exit(1)
 
 name_check('project','{{ cookiecutter.project }}')
-name_check('app','{{ cookiecutter.app }}')
 
 
 assert "\\" not in "{{ cookiecutter.author }}", "用户名不合法！"
+
+if {{ cookiecutter.use_celery }} and  not {{ cookiecutter.use_redis }}:
+    print(f"错误：使用Celery需要设置`use_redis`为`True`！")
+    sys.exit(1)
